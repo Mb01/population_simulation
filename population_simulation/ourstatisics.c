dfc670495b5b9ea809994890fb53ceb698fe52c1
Mark Bowden <Mbowden01@gmail.com>
2014-01-31 14:26:26 +0900

    Update ourstatisics.c

--- population_simulation/ourstatisics.c
+++ population_simulation/ourstatisics.c
@@ -1,8 +1,14 @@
 
-
+#include "stdlib.h"
+#include "ourstatistics.h"
 
 
 float rand_zero_one(){
 	float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 	return r;
+}
+
+float rand_int(int min, int max){
+	float r =  rand() % (max - min) + min;
+		return r;
 }
